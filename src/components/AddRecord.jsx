64a41adf94b095a539d96bf8eab719995ac78c4c
Mauricio Benavente <mauriciobenavente97@icloud.com>
2023-07-01 15:48:44 +0200

    working on connecting the api

--- src/components/AddRecord.jsx
+++ src/components/AddRecord.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-
-
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 function AddRecord() {
   const gotToken = localStorage.getItem("authToken");
 
- const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("");
   const [recordPath, setRecordPath] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
@@ -14,39 +13,39 @@ function AddRecord() {
   const handleTitle = (e) => setTitle(e.target.value);
   const handleRecordPath = (e) => setRecordPath(e.target.files[0]);
 
-
-  const handleAddRecord = (e) => {
+  const handleAddRecord = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-  formData.append("title", title);
-  formData.append("recordPath", recordPath);
+    formData.append("title", title);
+    formData.append("recordPath", recordPath);
+   
 
-  //Gets the value of the inputs so you can check them in the console
- /* const titleValue = formData.get("title"); 
-const recordPathValue = formData.get("recordPath");
+    try {
+      const response = await axios.post(
+        "http://localhost:5005/auth/addRecord",
+        formData,
+        {
+          headers: { authorization: `Bearer ${gotToken}` },
+        }
+      );
+      const { record } = response.data;
+      const transcriptionId = response.data.transcriptionId;
+      const generatedText = response.data.generatedText;
 
-console.log(titleValue); 
-console.log(recordPathValue);
-*/
-console.log("here is our token", gotToken)
-    axios
-      .post(`http://localhost:5005/auth/addRecord`, formData,   {
-        headers: { authorization: `Bearer ${gotToken}` },
-      })
-      .then(() => {
-        navigate("/recordsPage");
-        
-       console.log()
-      })
-      .catch((error) => {
-        const errorDescription = error.response.data.message;
-        setErrorMessage(errorDescription);
-      });
-      
+      // Handle the response data as needed
+      console.log("Record:", record);
+      console.log("Transcription ID:", transcriptionId);
+      console.log("Generated Text:", generatedText);
+
+      navigate("/recordsPage");
+    } catch (error) {
+      const errorDescription = error.response.data.message;
+      setErrorMessage(errorDescription);
+    }
   };
 
   return (
-    <div >
+    <div>
       <h1>Add Record</h1>
 
       <form onSubmit={handleAddRecord} enctype="multipart/form-data">
@@ -56,11 +55,8 @@ console.log("here is our token", gotToken)
         <label>Record:</label>
         <input
           type="file"
-         
           name="recordPath"
-          
           id="recordPath"
-          
           onChange={handleRecordPath}
         />
 
@@ -69,7 +65,6 @@ console.log("here is our token", gotToken)
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
       <p>Already have an account?</p>
-    
     </div>
   );
 }
