ac8cbac32d0fcab3007ac7f7101036b2495bcc32
“Zuzana <zuzana@getyourguide.com>
2023-07-22 21:19:57 +0200

    Added background color to body again

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,14 @@ function Layout({ children }) {
           <main>
             <nav>
             <div className="app-title"> <img className="star-small" src="https://upload.wikimedia.org/wikipedia/commons/1/10/Black_4_Point_Star.png" alt="four pointed star"/> Too Lazy to Type</div>
-            <ul className="nav-links" >
-              <li><a className="nav-link" href="/">Home</a></li>
-              <li><a className="nav-link" href="/signup">Register</a></li>
-              <li><a className="nav-link" href="/login">Login</a></li>
-              <li><a className="nav-link" href="/profile">Profile</a></li>
-              {/* TO DO - replace this link */}
-              <li><a className="special-nav-link" href="/">Create</a></li>
-            </ul>
+              <ul className="nav-links" >
+                <li><a className="nav-link" href="/">Home</a></li>
+                <li><a className="nav-link" href="/signup">Register</a></li>
+                <li><a className="nav-link" href="/login">Login</a></li>
+                <li><a className="nav-link" href="/profile">Profile</a></li>
+                {/* TO DO - replace this link */}
+                <li><a className="special-nav-link" href="/">Create</a></li>
+              </ul>
             </nav>
               {children}
           </main>    
