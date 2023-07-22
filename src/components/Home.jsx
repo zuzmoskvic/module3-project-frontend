f6c28cf6d633f3cb81e5e9d3d5138dd896b101f5
“Zuzana <zuzana@getyourguide.com>
2023-07-22 19:16:17 +0200

    Added image to Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,22 @@
 import Layout from "./Layout"
+import illustration from "../img/listening.png"
 
 function Home() {
   
   return (
     <div>
         <Layout>
-        <div className="home-leftdiv">
-          <h1 className="home-h1"> <img className="star" src="https://upload.wikimedia.org/wikipedia/commons/1/18/Four-point_star_%28fixed_width%29.svg" alt="four pointed star"/>   Generate Text from Speech</h1>
-          <button>Get started</button>
+        <div className="home-maindiv">
+            <div className="home-leftdiv">
+              <h1 className="home-h1"> <img className="star" src="https://upload.wikimedia.org/wikipedia/commons/1/18/Four-point_star_%28fixed_width%29.svg" alt="four pointed star"/>   Generate Text from Speech</h1>
+              <button>Get started</button>
+            </div>
+
+            <div className="home-rightdiv">
+              <img className="illustration" src={illustration} alt="listener illustration"/>
+            </div>
         </div>
-
         </Layout>
-
     </div>
   )
 }
