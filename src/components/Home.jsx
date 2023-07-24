cfda128c5ea2f07e86b3faa3a37cc00e8fbe1035
“Zuzana <zuzana@getyourguide.com>
2023-07-24 23:37:54 +0200

    /Home - added link to get started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import Layout from "./Layout"
 import illustration from "../img/listening.png"
+import { Link } from "react-router-dom"
 
 function Home() {
   
@@ -10,7 +11,7 @@ function Home() {
               <div className="home-leftdiv">
                 <h1 className="home-h1"> <img className="star" src="https://upload.wikimedia.org/wikipedia/commons/1/18/Four-point_star_%28fixed_width%29.svg" alt="four pointed star"/>   Generate Text from Speech</h1>
                 <h2 className="home-h2">We listen and you tell us what to write</h2>
-                <button className="pink-button">Get started</button>
+                <Link to="/signup"><button className="pink-button"> Get started</button></Link>
               </div>
 
               <div className="home-rightdiv">
