7510701030e6ea01785bad48af45aafe49b5dd70
“Zuzana <zuzana@getyourguide.com>
2023-07-25 00:02:09 +0200

    Clean up Profile page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -33,29 +33,8 @@ function ProfilePage() {
   }, [] );
 
   if (loading) {
-    // Render a loading message while fetching user data
     return <p>Loading...</p>;
   }
-
-  // const handleDeleteAccount = async () => {
-  //   const confirmDelete = window.confirm("Are you sure you want to delete your account?");
-  //   if (confirmDelete) {
-  //     try {
-  //       const gotToken = localStorage.getItem("authToken");
-  //       if (gotToken) {
-  //         await axios.delete(`${API_URL}/profile`, {
-  //           headers: { authorization: `Bearer ${gotToken}` },
-  //         });
-  //         removeToken();
-  //         setUser(null);
-  //         setIsLoggedIn(false);
-  //         navigate("/login");
-  //       }
-  //     } catch (err) {
-  //       console.log("There was an error while eliminating the user account", err);
-  //     }
-  //   }
-  // };
 
   return (
     <Layout>
@@ -70,27 +49,20 @@ function ProfilePage() {
 
           <div className="profile-leftdiv-buttonsdiv">
             <button className="blue-button"  onClick={ logOutUser }>Logout</button>
-            {/* <button className="red-button" onClick={ handleDeleteAccount }>Delete Account</button>  */}
             <Link to={`/editUser/${user._id}`}><button className="blue-button">Edit user</button></Link> 
             <Link to={`/deleteUser/${user._id}`}><button className="red-button" >Delete user</button></Link> 
           </div>
         </div>
 
         <div className="profile-rightdiv">
-             {/* <Link to={`/editUser/${user._id}`}><button>Edit your user</button></Link>   */}
-             {/* <Link to={`/deleteUser/${user._id}`}><button>Delete your user</button></Link>   */}
-
             <div className="profile-rightdiv-buttonsdiv">
               <Link to={ "/addRecord" }><button className="pink-button">Upload a recording</button></Link>
-              {/* <Link to={ "/transcribe" }><button className="pink-button">Add transcribe</button></Link> */}
               <Link to={ "/record" }><button className="pink-button">Make a recording</button></Link>
             </div>
 
             <div className="home-rightdiv">
               <Link to={ "/display" } className="login-link">Read your editing texts</Link>
             </div>
-           
-            {/* <Display/> */}
         </div>
 
       </div>
