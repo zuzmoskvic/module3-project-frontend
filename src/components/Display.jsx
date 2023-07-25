28665974e5f25d9daae12486be4fd6a7bc33dc98
“Zuzana <zuzana@getyourguide.com>
2023-07-25 21:58:10 +0200

    Further changes to display and delete buttons

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -28,33 +28,54 @@ function Display() {
       })
       .then((res) => {
         setDisplayedRecord(res.data);
-        console.log(res.data);
-        console.log("record zero", res.data[0].writtenText[0].text);
+        // console.log(res.data);
+        // console.log("record zero", res.data[0].writtenText[0].text);
         setFetching(false);
       })
       .catch((error) => console.log(error));
   }, [toggle]);
 
-  // const handleDeleteTranscription = async (transcriptId) => {
-  //   const confirmDelete = window.confirm(
-  //     "Are you sure you want to delete your transcription?"
-  //   );
-  //   if (confirmDelete && gotToken) {
-  //     try {
-  //       await axios.post(
-  //         `${API_URL}/auth/display`,
-  //         { transcriptId },
-  //         {
-  //           headers: { authorization: `Bearer ${gotToken}` },
-  //         }
-  //       );
-  //       setDisplayedRecord(prevRecords => prevRecords.filter(record => record !== transcriptId));
-  //       setToggle(((current) => !current))
-  //     } catch (err) {
-  //       console.log("There was an error while deleting the transcript", err);
-  //     }
-  //   }
-  // };
+  const handleDeleteTranscription = async (recordId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete your transcription?"
+    );
+    if (confirmDelete && gotToken) {
+      try {
+        await axios.post(
+          `${API_URL}/auth/display`,
+          { recordId },
+          {
+            headers: { authorization: `Bearer ${gotToken}` },
+          }
+        );
+        setDisplayedRecord(prevRecords => prevRecords.filter(record => record.recordId !== recordId));
+        setToggle(((current) => !current))
+      } catch (err) {
+        console.log("There was an error while deleting the transcript", err);
+      }
+    }
+  };
+
+  const handleDeleteWrittenText = async (writtenTextId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete your transcription?"
+    );
+    if (confirmDelete && gotToken) {
+      try {
+        await axios.post(
+          `${API_URL}/auth/deletetext`,
+          { writtenTextId },
+          {
+            headers: { authorization: `Bearer ${gotToken}` },
+          }
+        );
+        setDisplayedRecord(prevRecords => prevRecords.filter(record => record !== writtenTextId));
+        setToggle(((current) => !current))
+      } catch (err) {
+        console.log("There was an error while deleting the transcript", err);
+      }
+    }
+  };
 
   return (
     <Layout>
@@ -66,16 +87,18 @@ function Display() {
           <div>
             {displayedRecord.map((entry, index) => (
               <div key={index}>
-              <p>This is a transcript: {entry.transcript} </p>  
+              <p>This is a transcript: {entry.transcript} </p> 
+
+              {/* transcript delete */}
+              {console.log("entry._id", entry._id)}
+              <button className="red-button" onClick={() => handleDeleteTranscription(entry._id)}> Delete Transcription </button> 
                 {entry.writtenText.map((item, itemIndex) => (
                   <div key={itemIndex}>
                   <p>This is a written text:  {item.text} </p>
-                    <button
-                      className="red-button"
-                      // onClick={() => handleDeleteTranscription(item)}
-                    >
-                      Delete Transcription
-                    </button>
+                  {console.log("item id", item._id)}
+
+                {/* writtenText delete */}
+                    <button className="red-button" onClick={() => handleDeleteWrittenText(item._id)}> Delete written text </button>
                   </div>
                 ))}
               </div>
