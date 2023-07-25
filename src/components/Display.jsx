25836122795557e6c12ccc90e9480c5a5bd98aef
“Zuzana <zuzana@getyourguide.com>
2023-07-25 19:21:03 +0200

    /Display all transcriptions

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,39 +1,83 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from "./Layout";
+import { API_URL } from "../config/config.index";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Display() {
   const gotToken = localStorage.getItem("authToken");
-  // const [displayedText, setDisplayedText] = useState("");
-  const [displayedRecord, setDisplayedRecord] = useState(null);
-  const [fetching, setFetching] = useState(true);
+  const user = useContext(AuthContext);
+  const { userId } = useParams();
 
-  useEffect(() => {     
-    axios.get('http://localhost:5005/auth/display',
-    {
-      headers: { authorization: `Bearer ${gotToken}` },
-    })
-    .then((res) => {
-      // setDisplayedText(res.data);
-      setDisplayedRecord(res.data);
-      setFetching(false);
-    })
-    .catch((error) => console.log(error));
-  }, [] );
+  const [displayedRecord, setDisplayedRecord] = useState([]);
+  const [fetching, setFetching] = useState(true);
+  const [toggle, setToggle] = useState(false)
+
+  // const handleToggle = () => {
+  //   setToggle((current) => !current);
+  // };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5005/auth/display", {
+        headers: { authorization: ` Bearer ${gotToken} `},
+      })
+      .then((res) => {
+        setDisplayedRecord(res.data);
+        setFetching(false);
+      })
+      .catch((error) => console.log(error));
+  }, [toggle]);
+
+  const handleDeleteTranscription = async (transcriptId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete your transcription?"
+    );
+    if (confirmDelete && gotToken) {
+      try {
+        await axios.post(
+          `${API_URL}/auth/display`,
+          { transcriptId },
+          {
+            headers: { authorization: `Bearer ${gotToken}` },
+          }
+        );
+        setDisplayedRecord(prevRecords => prevRecords.filter(record => record !== transcriptId));
+        setToggle(((current) => !current))
+      } catch (err) {
+        console.log("There was an error while deleting the transcript", err);
+      }
+    }
+  };
 
   return (
     <Layout>
       <div className="display-main-div">
         <h3 className="small-h3">Transcript:</h3>
-        { fetching ? <li> Loading transcript...</li> : <li> { displayedRecord.transcript } </li>}
-
-        <h3 className="small-h3">Written text:</h3>
-      {fetching ? <li>Loading written text...</li> : displayedRecord.writtenText.map((entry) => (
-        <li key={entry._id}>{entry.text}</li>
-      ))}
+        {fetching ? (
+          <li>Loading transcript...</li>
+        ) : (
+          <div>
+            {displayedRecord.map((entry, index) => (
+              <div key={index}>
+                {entry}
+                <button
+                  className="red-button"
+                  onClick={() => handleDeleteTranscription(entry)}
+                >
+                  Delete Transcription
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Display
+export default Display;
