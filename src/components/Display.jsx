a364314f7327c305a37807536041958f34745cdb
“Zuzana <zuzana@getyourguide.com>
2023-07-25 20:26:05 +0200

    /Display array of transcripts and writtentexts

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -28,31 +28,33 @@ function Display() {
       })
       .then((res) => {
         setDisplayedRecord(res.data);
+        console.log(res.data);
+        console.log("record zero", res.data[0].writtenText[0].text);
         setFetching(false);
       })
       .catch((error) => console.log(error));
   }, [toggle]);
 
-  const handleDeleteTranscription = async (transcriptId) => {
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete your transcription?"
-    );
-    if (confirmDelete && gotToken) {
-      try {
-        await axios.post(
-          `${API_URL}/auth/display`,
-          { transcriptId },
-          {
-            headers: { authorization: `Bearer ${gotToken}` },
-          }
-        );
-        setDisplayedRecord(prevRecords => prevRecords.filter(record => record !== transcriptId));
-        setToggle(((current) => !current))
-      } catch (err) {
-        console.log("There was an error while deleting the transcript", err);
-      }
-    }
-  };
+  // const handleDeleteTranscription = async (transcriptId) => {
+  //   const confirmDelete = window.confirm(
+  //     "Are you sure you want to delete your transcription?"
+  //   );
+  //   if (confirmDelete && gotToken) {
+  //     try {
+  //       await axios.post(
+  //         `${API_URL}/auth/display`,
+  //         { transcriptId },
+  //         {
+  //           headers: { authorization: `Bearer ${gotToken}` },
+  //         }
+  //       );
+  //       setDisplayedRecord(prevRecords => prevRecords.filter(record => record !== transcriptId));
+  //       setToggle(((current) => !current))
+  //     } catch (err) {
+  //       console.log("There was an error while deleting the transcript", err);
+  //     }
+  //   }
+  // };
 
   return (
     <Layout>
@@ -64,13 +66,18 @@ function Display() {
           <div>
             {displayedRecord.map((entry, index) => (
               <div key={index}>
-                {entry}
-                <button
-                  className="red-button"
-                  onClick={() => handleDeleteTranscription(entry)}
-                >
-                  Delete Transcription
-                </button>
+              <p>This is a transcript: {entry.transcript} </p>  
+                {entry.writtenText.map((item, itemIndex) => (
+                  <div key={itemIndex}>
+                  <p>This is a written text:  {item.text} </p>
+                    <button
+                      className="red-button"
+                      // onClick={() => handleDeleteTranscription(item)}
+                    >
+                      Delete Transcription
+                    </button>
+                  </div>
+                ))}
               </div>
             ))}
           </div>
@@ -78,6 +85,6 @@ function Display() {
       </div>
     </Layout>
   );
-}
-
+  
+                }
 export default Display;
