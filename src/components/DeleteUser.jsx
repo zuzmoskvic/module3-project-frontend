b4f6ad5983a00e2f5bdb6805ab49c072dbafe5c2
“Zuzana <zuzana@getyourguide.com>
2023-07-28 20:33:26 +0200

    Fixed delete user

--- src/components/DeleteUser.jsx
+++ src/components/DeleteUser.jsx
@@ -1,67 +1,38 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../config/config.index";
 import Layout from "./Layout";
 
 function DeleteUser() {
-  const { user, logOutUser } = useContext(AuthContext);
+  const { logOutUser } = useContext(AuthContext);
   const { userId } = useParams();
-
   const gotToken = localStorage.getItem("authToken");
-  const [errorMessage, setErrorMessage] = useState("");
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    try {
-      if (gotToken) {
-        axios
-          .get(`${API_URL}/auth/deleteUser/${userId}`, {
-            headers: { authorization: `Bearer ${gotToken}` },
-          })
-          .then(() => {})
-          .catch((error) => {
-            console.log(error);
-            setErrorMessage("Failed to fetch user data. Please try again.");
-          });
-      }
-    } catch (error) {
-      console.log(error);
-      setErrorMessage("Failed to fetch user data. Please try again.");
-    }
-  }, []);
-
-  const handleDeleteAccount = async (e) => {
-    e.preventDefault();
-    const requestBody = { email: user.email };
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete your account?"
-    );
-    if (confirmDelete && gotToken) {
-      try {
-        await axios.post(`${API_URL}/auth/deleteUser/${userId}`, requestBody, {
-          headers: { authorization: `Bearer ${gotToken}` },
-        });
-        localStorage.removeItem('authToken');
-        logOutUser();
-        navigate("/signup");
-      } catch (err) {
-        console.log(
-          "There was an error while eliminating the user account",
-          err
-        );
-      }
+  const handleDeleteAccount = () => {
+    const confirmed = window.confirm("Are you sure you want to delete your account?");
+    if (confirmed) {
+      axios
+        .delete(`${API_URL}/auth/deleteUser/${userId}`, {
+          headers: { Authorization: `Bearer ${gotToken}` },
+        })
+        .then(() => {
+          localStorage.removeItem('authToken');
+          logOutUser();
+          navigate("/");
+        })
+        .catch((err) => console.log(err));
     }
   };
+  
 
   return (
     <Layout>
       <div>
-        <button className="red-button" onClick={handleDeleteAccount}>
-          Delete Account
-        </button>
+        <button className="red-button" onClick={handleDeleteAccount}>Delete Account</button>
       </div>
     </Layout>
   );
