d79c60c4d1fe21d0e413993d706bfb1c9d961ed9
“Zuzana <zuzana@getyourguide.com>
2023-07-28 18:51:18 +0200

    Delete Transcribe element

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import AddRecord from "./components/AddRecord";
 import RecordsPage from "./components/RecordsPage"
 import ProfilePage from "./components/ProfilePage";
 import PrivatePage from "./components/PrivatePage";
-import Transcribe from "./components/Transcribe";
 import WritenTextPage from "./components/WritenTextPage";
 import EditUser from "./components/EditUser";
 import Home from "./components/Home"
@@ -23,16 +22,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/addRecord"  element={<AddRecord />}/>
-        <Route path="/transcribe"  element={<Transcribe />}/>
         <Route path="/recordsPage" element={<RecordsPage />} />
         <Route path="/write" element={<WritenTextPage />} />
         <Route path="/record" element={<Recorder />} />
         <Route path="/display" element={<Display />} />
         <Route path="/editUser/:userId" element={<EditUser />} />
         <Route path="/deleteUser/:userId" element={<DeleteUser />} />
-
          <Route path="/profile" element={<PrivatePage> <ProfilePage /></PrivatePage>}/> 
-         { /* <Route path="/profile" element={<ProfilePage/>}/> */}
       </Routes>
     </div>
   );
