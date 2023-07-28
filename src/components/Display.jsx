1a4ba8d86a5b065c207eac09d950759734762b78
“Zuzana <zuzana@getyourguide.com>
2023-07-28 16:54:58 +0200

    Added sad image when zero entries

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,22 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from "./Layout";
 import { API_URL } from "../config/config.index";
-import { useContext } from "react";
-import { AuthContext } from "../context/auth.context";
-import { useNavigate, useParams } from "react-router-dom";
-import { useRef } from "react";
+import sadUrl from "../img/sad.jpeg";
+import { Link } from "react-router-dom";
 
 function Display() {
   const gotToken = localStorage.getItem("authToken");
-  const ref = useRef(null);
-  const user = useContext(AuthContext);
-  const { userId } = useParams();
-
   const [displayedRecord, setDisplayedRecord] = useState([]);
   const [fetching, setFetching] = useState(true);
   const [toggle, setToggle] = useState(false);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -66,47 +58,65 @@ function Display() {
   return (
     <Layout>
       <div className="display-div">
-        <div className="display-main-div-1">
-          {fetching ? (
-            <li>Loading transcript...</li>
-          ) : (
-            <div className="test">
-              {displayedRecord.reverse().map((entry, index) => (
-                <div key={index} id={totalRecords - index - 1}>
-                  <h3> Recording #{totalRecords - index} {entry.title}</h3>
-                  <li>
-                    <span className="bold">Transcript: </span>{entry.transcript}
-                  </li>
-                  {entry.writtenText.map((item, itemIndex) => (
-                    <div key={itemIndex}>
+
+      {totalRecords === 0 ? (
+          <div className="sad-image-div">
+            <img className="sad-url" src={sadUrl} alt="sad"/> 
+            <p className="bold">Nothing to see here yet. 😔</p>
+            <Link to="/profile"><button className="pink-button"> Record something</button></Link>
+          </div>
+        ) : (
+          <>
+
+          <div className="display-main-div-1">
+              {fetching ? (
+                <li>Loading transcript...</li>
+              ) : (
+                <div className="test">
+                  {displayedRecord.reverse().map((entry, index) => (
+                    <div key={index} id={totalRecords - index - 1}>
+                      <h3>
+                        Recording #{totalRecords - index} {entry.title}
+                      </h3>
                       <li>
-                        <span className="bold">Written text: </span>{item.text}
+                        <span className="bold">Transcript: </span>
+                        {entry.transcript}
                       </li>
-                      <button
-                        className="red-button"
-                        onClick={() => handleDeleteTranscription(entry._id)}
-                      >
-                        Delete
-                      </button>
+                      {entry.writtenText.map((item, itemIndex) => (
+                        <div key={itemIndex}>
+                          <li>
+                            <span className="bold">Written text: </span>
+                            {item.text}
+                          </li>
+                          <button
+                            className="red-button"
+                            onClick={() => handleDeleteTranscription(entry._id)}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      ))}
                     </div>
                   ))}
                 </div>
+              )}
+            </div>
+
+            <div className="display-main-div-2">
+              <p className="bold">Jump to section:</p>
+              {displayedRecord.map((entry, index) => (
+                <button
+                  className="simple-links"
+                  onClick={() => handleLinkClick(index)}
+                >
+                  <li>
+                    Recording #{totalRecords - index} {entry.title}
+                  </li>
+                </button>
               ))}
             </div>
-          )}
-        </div>
-
-        <div className="display-main-div-2">
-          <p className="bold">Jump to section:</p>
-          {displayedRecord.map((entry, index) => (
-            <button
-              className="simple-links"
-              onClick={() => handleLinkClick(index)}
-            >
-              <li>Recording #{totalRecords - index} {entry.title}</li>
-            </button>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </Layout>
   );
