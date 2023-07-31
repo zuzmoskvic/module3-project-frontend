452e87213fa5221ab6d1c2ae03bf87eb5e6ed1ff
“Zuzana <zuzana@getyourguide.com>
2023-07-31 20:17:28 +0200

    Profile image clickable

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -44,7 +44,7 @@ function ProfilePage() {
           <h1 className="profile-h1">Your profile</h1>
 
           {/* Clickable user profile  */}
-          <Link to={`editUser/${user._id}`}  className="user-div-link">
+          <Link to={`/editUser/${user._id}`}  className="user-div-link">
             <div className="user-div">
               {imageUrl ? (<img src={imageUrl} alt="user profile" className="profile-image"/>) : (<img src={defaultImageUrl} alt="user profile" className="profile-image"/>)}
               <p className="profile-email">{email}</p>
